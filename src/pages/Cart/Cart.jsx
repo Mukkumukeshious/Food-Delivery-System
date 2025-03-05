6705912df876c705f92378e76ec8e71e85fda582
unknown <iammukeshious@gmail.com>
2025-03-05 13:53:20 +0545

    adding checkout functionality on cart page

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -38,6 +38,39 @@ const Cart = () => {
         }
 
       </div>
+      <div className="cart-bottom">
+        <div className="cart-total">
+          <div className="total">
+            <div className="cart-total-details">
+              <p>Subtotal</p>
+              <p>{0}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <p>Dlivery Fee</p>
+              <p>{2}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <b>Total</b>
+              <p><b>{0}</b></p>
+            </div>
+            <button>PROCEED TO CHECKOUT</button>
+          </div>
+        
+        </div>
+        <div className="cart-promocode">
+            <div>
+              <p>If you have promo code, Applied it here</p>
+              <div className='promo-cart-input'>
+                <input type="text" placeholder='promo code' />
+                <button>Submit</button>
+
+              </div>
+            </div>
+
+          </div>
+      </div>
       
     </div>
   )
